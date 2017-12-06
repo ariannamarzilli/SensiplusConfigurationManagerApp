4dbeb973cb4d98f86b4e9892a86ae91b6e5b6222
mariangela01 <mariangela.evangelista@studentmail.unicas.it>
2017-12-06 18:39:59 +0100

    Modifiche interfaccia

--- src/it/unicas/project/view/SensingElementDetailsController.java
+++ src/it/unicas/project/view/SensingElementDetailsController.java
@@ -8,7 +8,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class SensingElementDetailsController {
